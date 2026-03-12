761812b8bbb17028f220227be6cd7a440fa2ead9
dcpati <delacruzpats.g@gmail.com>
2026-03-12 22:37:15 +0800

    fix: resolve hydration error from nested layout html tags

--- gdgoc/src/app/(main)/layout.tsx
+++ gdgoc/src/app/(main)/layout.tsx
@@ -1,35 +1,12 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import NavBar from "./components/NavBar";
-import "../globals.css";
 import { ReactNode } from "react";
+import NavBar from "./components/NavBar";
 import { UserProvider } from "@/app/(main)/components/UserProvider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "GDGoC Mapua University",
-  description: "Google Developers Group on Campus Mapua University is a student-led organization dedicated to fostering a community of developers and tech enthusiasts at Mapua University. We aim to provide a platform for learning, collaboration, and innovation in the field of technology. Our mission is to empower students with the skills and knowledge needed to succeed in the tech industry while promoting diversity and inclusion within our community.",
-}
-
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function MainLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased pt-32`}
-      >
-        <UserProvider>
-        <NavBar />
-        {children}
-        </UserProvider>
-      </body>
-    </html>
-  )};
+    <UserProvider>
+      <NavBar />
+      {children}
+    </UserProvider>
+  );
+}
--- gdgoc/src/app/layout.tsx
+++ gdgoc/src/app/layout.tsx
@@ -15,8 +15,8 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "GDGoC Mapua University",
-  description: "Google Developers Group on Campus Mapua University is a student-led organization dedicated to fostering a community of developers and tech enthusiasts at Mapua University. We aim to provide a platform for learning, collaboration, and innovation in the field of technology. Our mission is to empower students with the skills and knowledge needed to succeed in the tech industry while promoting diversity and inclusion within our community.",
-}
+  description: "Google Developers Group on Campus Mapua University",
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
@@ -27,4 +27,5 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         {children}
       </body>
     </html>
-  )};
+  );
+}
