e4cf34d10377ea2b7e1e0d97b68ca23969772a9a
BaghettiGH <patrickmolina042@gmail.com>
2026-04-30 12:20:34 +0800

    mirror repo to gdgoc

--- gdgoc/src/app/(auth)/login/page.tsx
+++ gdgoc/src/app/(auth)/login/page.tsx
@@ -13,7 +13,6 @@ export default function LoginPage() {
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
-
   const handleSignIn = async (e: React.FormEvent) => {
     e.preventDefault();
     setLoading(true);
