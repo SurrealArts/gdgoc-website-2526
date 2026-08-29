d962693d794b99c3572a6c313929810a07bd968f
SurrealArts <heiwamakina@gmail.com>
2026-08-29 16:38:54 +0800

    feat: Add docker image github action

--- gdgoc/next.config.js
+++ gdgoc/next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   // Add this images block to whitelist your Supabase URL
   images: {
     remotePatterns: [
@@ -14,4 +13,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
